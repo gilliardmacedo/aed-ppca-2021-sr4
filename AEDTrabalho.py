61d3df213ab723ccc4658bfa07cb6d9d7aadac70
Gilliard Macedo <gilliardmacedo@gmail.com>
2021-08-22 20:21:37 -0300

    ajuste do problema

--- AEDTrabalho.py
+++ AEDTrabalho.py
@@ -1,11 +1,13 @@
 import itertools
 from math import fsum
+import time
+start_time = time.time()
 
 def carregaR():
     inputMsg = ("Informe as probabilidades de acesso de cada registro em uma linha, separadas por espaços.\n"
     "Lembre-se de que a soma das probabilidades deve ser igual a 1.\n")
     R = list(float(prob) for prob in input(inputMsg).strip().split())
-    somaProbability = sum(R)
+    somaProbability = fsum(R)
     if somaProbability != 1:
         raise ValueError("A soma das probabilidades deve ser igual a 1")
     return R
@@ -98,11 +100,14 @@ obterCombinacoes(todasCombinacoes, m, solucaoAtual, indicesJaEscolhidos,
 print("Encontradas %d possíveis combinações \n" % len(todasCombinacoes))
 
 indicesM = [i for i in range(1, m + 1)]
+# Combinacoes simples
 possiveisParesEmM = [pair for pair in itertools.combinations(indicesM, 2)]
+# Combinações inversas
+possiveisParesEmM.extend([(y, x) for (x, y) in possiveisParesEmM])
+# Combinaçoes do mesmo indice
+possiveisParesEmM.extend([(i, i) for i in indicesM])
 
 custoLatenciaSolucao = 999999
-indMaiorCustoISolucao = 0
-indMaiorCustoJSolucao = 0
 combinacaoSolucao = None
 # Cada uma das combinacoes nesse ponto é uma possivel organizacao dos m
 # setores, com m listas contendo os indices dos registros na lista/array
@@ -110,11 +115,12 @@ combinacaoSolucao = None
 # indica que o setor 1 está com os itens de indices 3 e 4 da entrada, ou seja,
 # o 4o e o 5o item(por causa dos indices em Python começando de zero)
 for combinacao in todasCombinacoes:
-    maiorCustoLatenciaCombinacao = -1
+    custoTotalCombinacao = 0
     indMaiorCustoICombinacao = 0
     indMaiorCustojCombinacao = 0
+    
     # Aqui deve ser verificado o custo de latencia dessa combinacao
-    # para cada par possivel i e j.
+    # para cada par possivel i e j e acumulado.
     for par in possiveisParesEmM:
         i = par[0]
         j = par[1]
@@ -123,32 +129,20 @@ for combinacao in todasCombinacoes:
         probI = somarProbabilidades(combinacao[i - 1], R)
         probJ = somarProbabilidades(combinacao[j - 1], R)
         valorIJ = probI * probJ * distancia(i, j, m)
-        valorJI = probI * probJ * distancia(j, i, m)
-        if valorIJ > maiorCustoLatenciaCombinacao:
-            maiorCustoLatenciaCombinacao = valorIJ
-            indMaiorCustoICombinacao = i
-            indMaiorCustoJCombinacao = j
-        if valorJI > maiorCustoLatenciaCombinacao:
-            maiorCustoLatenciaCombinacao = valorJI
-            indMaiorCustoICombinacao = j
-            indMaiorCustoJCombinacao = i
-            
+        custoTotalCombinacao += valorIJ
+    
     # Verifica se essa combinacao foi a de menor custo entre todas
-    if maiorCustoLatenciaCombinacao < custoLatenciaSolucao:
-        print("Nova solução candidata encontrada")
-        print(combinacao)
-        custoLatenciaSolucao = maiorCustoLatenciaCombinacao
-        indMaiorCustoISolucao = indMaiorCustoICombinacao
-        indMaiorCustoJSolucao = indMaiorCustoJCombinacao
+    if custoTotalCombinacao < custoLatenciaSolucao:
+        custoLatenciaSolucao = custoTotalCombinacao
         combinacaoSolucao = combinacao
     
 print("\n\n=== Solução ===")
 print("Número de registros: %d " % len(R))
 print("Disposição dos registros: ")
 print(combinacaoSolucao)
-print("O maior custo de latência encontrado nessa solução foi %f " % custoLatenciaSolucao)
-print("O valor foi encontrado para i = %d e j = %d" % (indMaiorCustoISolucao, indMaiorCustoJSolucao))
+print("O custo total de latência dessa solução é %f " % custoLatenciaSolucao)
 if custoLatenciaSolucao <= k:
     print("Esse valor é MENOR ou IGUAL a K = %d" % k)
 else:
     print("Esse valor é MAIOR que K = %d" % k)
+print("--- Tempo de execução: %s segundos ---" % (time.time() - start_time))
